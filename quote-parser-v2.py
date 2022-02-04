daa68464512302731c0249c2dae922b297b68c4f
Konstantin <ke_komandin@bnipi.rosneft.ru>
2022-02-04 15:41:42 +0500

    Рефакторинг: добавлены топ тэги, информация о сайте, исправлены относительные ссылки на абсолютные в тэгах.

--- quote-parser-v2.py
+++ quote-parser-v2.py
@@ -19,19 +19,33 @@ def dump_data(filename, result):
 
     df2 = pd.DataFrame(result['quotes'], columns=['Author', 'Quote', 'Tags'])
     df3 = pd.DataFrame(result['authors'], columns=['Author', 'Born', 'Location', 'Description', 'url'])
+    df4 = pd.DataFrame(result['top-tags'], columns=['Tag', 'Url'])
+    df5 = pd.DataFrame({'Parameter': list(result['site-info'].keys()),
+                        'Value': list(result['site-info'].values())})
 
     writer = pd.ExcelWriter(filename, engine='xlsxwriter')
     df1.to_excel(writer, sheet_name='tags', index=False)
     df2.to_excel(writer, sheet_name='quotes', index=False)
     df3.to_excel(writer, sheet_name='authors', index=False)
+    df4.to_excel(writer, sheet_name='top-tags', index=False)
+    df5.to_excel(writer, sheet_name='site-info', index=False)
     writer.save()
 
 
-def parse_tags(tags, result):
+def get_top_tags(tags, base_url):
+    keywords = []
+    for item in tags:
+        tag = item.find('a')
+        keywords.append((tag.text, base_url + tag.attrib['href']))
+
+    return keywords
+
+
+def parse_tags(tags, result, base_url):
     keywords = []
     for tag in tags:
         keywords.append(tag.text)
-        result[tag.text] = tag.attrib['href']
+        result[tag.text] = base_url + tag.attrib['href'][:-8]
     return ','.join(keywords)
 
 
@@ -61,14 +75,38 @@ def check_author_lnk(authors, lnk):
     return False
 
 
+def get_site_info(elem):
+    result = {}
+    quoted = elem.find_class('text-muted').pop()
+    if quoted is not None:
+        lnk = quoted.find('a')
+        if lnk is not None:
+            result[quoted.text.strip()] = lnk.attrib['href']
+    copyright_elem = elem.find_class('copyright').pop()
+    if copyright_elem is not None:
+        lnk = copyright_elem.find('a')
+        if lnk is not None:
+            result[copyright_elem.text.strip()] = lnk.attrib['href']
+
+    return result
+
+
 def parse(base_url, user_agent_gen, page_num):
     result = dict()
     result['tags'] = {}
     result['quotes'] = []
     result['authors'] = []
+    result['top-tags'] = []
+    result['site-info'] = {}
 
     url = f"{base_url}/page/{page_num}/"
     page = get_page_by_url(url, user_agent_gen)
+    top_tags = page.find_class('tag-item')
+    result['top-tags'] = get_top_tags(top_tags, base_url)
+    elem = page.find_class('footer').pop()
+    if elem is not None:
+        result['site-info'] = get_site_info(elem)
+
     quoutes = page.xpath('//div[@class="quote"]')
     if not quoutes:
         return None
@@ -77,7 +115,7 @@ def parse(base_url, user_agent_gen, page_num):
         author_title = None
         quote_text = None
         tags = quote.find_class('tag')
-        keywords = parse_tags(tags, result['tags'])
+        keywords = parse_tags(tags, result['tags'], base_url)
         text = quote.find_class('text')
         if text:
             quote_text = text[0].text
@@ -105,7 +143,15 @@ def prepare_data(data):
     result['tags'] = {}
     result['quotes'] = []
     result['authors'] = []
+    result['top-tags'] = []
+    result['site-info'] = {}
     for chunk in data:
+        if not result['site-info']:
+            result['site-info'] = chunk['site-info']
+
+        if not result['top-tags']:
+            result['top-tags'] = chunk['top-tags']
+
         tags = chunk['tags']
         for tag in tags.keys():
             if tag not in result['tags']:
@@ -135,15 +181,13 @@ if __name__ == '__main__':
     start_time = time.time()
     CONNECTIONS = 10
     ua = UserAgent()
-    data = []
-    pages = [n for n in range(1, 11)]
+    pages = [n for n in range(1, 11)]  # грязный хак конечно же...
     with concurrent.futures.ThreadPoolExecutor(max_workers=CONNECTIONS) as executor:
         res = executor.map(workder, pages)
 
-    data = [x for x in list(res) if x] # чистим список от None
+    data = [x for x in list(res) if x]  # чистим список от None
     print("Parse data: %s seconds" % (time.time() - start_time))
     result = prepare_data(data)
     print("Prepare data: %s seconds" % (time.time() - start_time))
     dump_data('dump-data2.xlsx', result)
     print("Save data: %s seconds" % (time.time() - start_time))
-
